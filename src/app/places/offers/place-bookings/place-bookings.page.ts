fd8b2e71d1539ea53bf853e27ff8a8d519e996e8
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-03 14:46:03 +0100

    same as previeus

--- src/app/places/offers/place-bookings/place-bookings.page.ts
+++ src/app/places/offers/place-bookings/place-bookings.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-place-bookings',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceBookingsPage implements OnInit {
 
-  constructor() { }
+  place?: Place
+
+  constructor(
+    private route: ActivatedRoute,
+    private navCtr: NavController,
+    private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('placeId')){
+        this.navCtr.navigateBack('/places/tabs/offers')
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId')!);
+    })
   }
 
 }
