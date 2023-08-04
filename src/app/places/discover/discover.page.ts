b61f7402d1b1b8d636c60787c03fe127046e6f32
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-04 10:29:19 +0100

    add segmentss

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { SegmentChangeEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-discover',
@@ -10,11 +11,17 @@ import { Place } from '../place.model';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[] = [];
+  ev? : CustomEvent<SegmentChangeEventDetail>;
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+  }
+
+  onFilterUpdate(event: Event){
+    this.ev = (event as CustomEvent<SegmentChangeEventDetail>);
+    console.log(this.ev.detail)
   }
 
 }
